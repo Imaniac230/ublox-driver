d3785a1cec69ee6ca74467fa603cc24e32a50b98
Imaniac230 <44968160+Imaniac230@users.noreply.github.com>
2024-05-03 20:15:37 +0200

    fix: Expose the buffer size, fix problems found in tests, add todo comments.

--- include/util/RingBuffer.hpp
+++ include/util/RingBuffer.hpp
@@ -6,6 +6,7 @@
 
 #include <util/Traits.h>
 
+//FIXME(buffer-traits): incompatible with random-access lists or empty iterables for now
 template<typename T, Traits::IterableWith<T> Data>
 class RingBuffer {
 public:
@@ -15,16 +16,19 @@ public:
         : buffer(std::move(other.buffer)), activeElements(other.activeElements), readIndex(other.readIndex),
           writeIndex(other.writeIndex) {}
 
-    [[nodiscard]] uint16_t capacity() const { return buffer.size() - activeElements; }
+    [[nodiscard]] uint16_t size() const { return buffer.size(); }
+    [[nodiscard]] uint16_t capacity() const { return size() - activeElements; }
     [[nodiscard]] uint16_t getActiveElements() const { return activeElements; }
-    [[nodiscard]] bool full() const { return activeElements == buffer.size(); }
+    [[nodiscard]] bool full() const { return activeElements == size(); }
     [[nodiscard]] bool empty() const { return activeElements == 0; }
 
+    //FIXME(buffer-traits): incompatible with random-access lists for now
     template<Traits::IterableWith<T> InputData>
     [[nodiscard]] inline bool write(InputData data) {
         if (data.size() > capacity()) return false;
+        if (data.size() == 0) return false;
 
-        const uint16_t overflowLimit = buffer.size() - writeIndex;
+        const uint16_t overflowLimit = size() - writeIndex;
         if (data.size() <= overflowLimit) {
             std::move(data.begin(), data.end(), buffer.begin() + writeIndex);
         } else {
@@ -32,37 +36,34 @@ public:
             std::move(data.begin() + overflowLimit, data.end(), buffer.begin());
         }
 
-        writeIndex = (writeIndex + data.size()) % buffer.size();
+        writeIndex = (writeIndex + data.size()) % size();
         activeElements += data.size();
         return true;
     }
-    //FIXME: incompatible with static arrays for now
+    //FIXME(buffer-traits): incompatible with static arrays for now
     template<Traits::IterableWith<T> OutputSlice>
     [[nodiscard]] inline std::optional<OutputSlice> read(uint16_t range) {
         if (range > activeElements) return std::nullopt;
+        if (range == 0) return std::nullopt;
 
         OutputSlice data{};
         if (data.size() != range) data.resize(range);
-        const uint16_t overflowLimit = buffer.size() - readIndex;
-        //TODO: decide if it is really safe to move out of the buffer here
+        const uint16_t overflowLimit = size() - readIndex;
         if (range <= overflowLimit) {
             std::move(buffer.begin() + readIndex, buffer.begin() + readIndex + range, data.begin());
-            //            data.insert(data.end(), buffer.begin() + readIndex, buffer.begin() + readIndex + range);
         } else {
             std::move(buffer.begin() + readIndex, buffer.begin() + readIndex + overflowLimit, data.begin());
             std::move(buffer.begin(), buffer.begin() + range - overflowLimit, data.begin());
-            //            data.insert(data.end(), buffer.begin() + readIndex, buffer.begin() + readIndex + overflowLimit);
-            //            data.insert(data.end(), buffer.begin(), buffer.begin() + range - overflowLimit);
         }
 
-        readIndex = (readIndex + range) % buffer.size();
+        readIndex = (readIndex + range) % size();
         activeElements -= range;
         return data;
     }
     [[nodiscard]] std::optional<T> tailOffset(uint16_t offset = 0) const {
         if (offset > activeElements) return std::nullopt;
 
-        return buffer[(readIndex + offset) % buffer.size()];
+        return buffer[(readIndex + offset) % size()];
     }
 
 private:
